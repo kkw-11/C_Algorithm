f75f074bd79dc5728458d9fcbe3008744ae5684a
asus <kkw-11@naver.com>
2021-04-05 14:32:27 +0900

    4/5

--- ex03/Main.cpp
+++ ex03/Main.cpp
@@ -1,47 +1,690 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
+template <typename T>
+class Array
+{
+private:
+    T* arr;
+    int size;
+    int capacity;
+public:
+    void Add(const T& data)
+    {
+        arr[size++] = data;
+    }
+    int Size()const
+    {
+        return size;
+    }
+    const T& At(int idx)const
+    {
+        return arr[idx];
+    }
+    void Remove(int idx)
+    {
+        for (int i = idx; i < size - 1; ++i)
+            arr[i] = arr[i + 1];
+        --size;
+    }
+    Array(int cap) // 持失切
+    {
+        size = 0;
+        capacity = cap;
+        arr = new T[cap];
+    }
+    ~Array()// 社瑚切
+    {
+        delete[] arr;
+    }
+};
+int main()
+{
+    Array<double> arr(100);
 
-	int* arr = new int[5];
-	
-	for (int i = 0; i < 5; ++i) {
-		arr[i] = 10 * (i + 1);
-	}
+    arr.Add(1.1);
+    arr.Add(2.34);
+    arr.Add(3.5678);
 
-	for (int i = 0; i < 5; ++i) {
-		printf("%d\n", arr[i]);
-	}
-
-	delete[] arr;
-	system("pause");
+    for (int i = 0; i < arr.Size(); ++i)
+        printf("%g\n", arr.At(i));
+    system("pause");
 }
 
+//#include <cstdio>
+//#include <cstdlib>
+//
+//template <typename T>
+//class Array
+//{
+//private:
+//    T* arr;
+//    int size;
+//    int capacity;
+//public:
+//    void Add(T data)
+//    {
+//        arr[size++] = data;
+//    }
+//    int Size()
+//    {
+//        return size;
+//    }
+//    T At(int idx)
+//    {
+//        return arr[idx];
+//    }
+//    void Remove(int idx)
+//    {
+//        for (int i = idx; i < size - 1; ++i)
+//            arr[i] = arr[i + 1];
+//        --size;
+//    }
+//    Array(int cap) // 持失切
+//    {
+//        size = 0;
+//        capacity = cap;
+//        arr = new T[cap];
+//    }
+//    ~Array()// 社瑚切
+//    {
+//        delete[] arr;
+//    }
+//};
+//int main()
+//{
+//    Array<double> arr(100);
+//
+//    arr.Add(1.1);
+//    arr.Add(2.34);
+//    arr.Add(3.5678);
+//
+//    for (int i = 0; i < arr.Size(); ++i)
+//        printf("%g\n", arr.At(i));
+//    system("pause");
+//}
 
 //#include <cstdio>
 //#include <cstdlib>
-//void ArrayTest(){
-//	int arr[5] = { 10,20,30,40,50 };
-//	for (int i = 0; i < 5; ++i) {
-//		printf("%d\n", arr[i]);
-//	}
-//}
-//int main() {
 //
-//	ArrayTest();
-//	ArrayTest();
-//	ArrayTest();
+//class Array
+//{
+//private:
+//    int* arr;
+//    int size;
+//    int capacity;
+//public:
+//    void Add(int data)
+//    {
+//        arr[size++] = data;
+//    }
+//    int Size()
+//    {
+//        return size;
+//    }
+//    int At(int idx)
+//    {
+//        return arr[idx];
+//    }
+//    void Remove(int idx)
+//    {
+//        for (int i = idx; i < size - 1; ++i)
+//            arr[i] = arr[i + 1];
+//        --size;
+//    }
+//    Array(int cap) // 持失切
+//    {
+//        size = 0;
+//        capacity = cap;
+//        arr = new int[cap];
+//    }
+//    ~Array()// 社瑚切
+//    {
+//        delete[] arr;
+//    }
+//};
+//int main()
+//{
+//    Array arr(100);
 //
+//    arr.Add(10);
+//    arr.Add(20);
+//    arr.Add(30);
 //
-//	system("pause");
+//    arr.Remove(1);
+//    for (int i = 0; i < arr.Size(); ++i)
+//        printf("%d\n", arr.At(i));
+//    system("pause");
 //}
 
 //#include <cstdio>
 //#include <cstdlib>
-//int main() {
-	//int arr[5] = { 10,20,30,40,50 };
-	//for (int i = 0; i < 5; ++i) {
-	//	printf("%d\n", arr[i]);
-	//}
-//	system("pause");
+//
+//class Array
+//{
+//private:
+//    int* arr;
+//    int size;
+//    int capacity;
+//public:
+//    void AddArray(int data)
+//    {
+//        arr[size++] = data;
+//    }
+//    int SizeArray()
+//    {
+//        return size;
+//    }
+//    int AtArray(int idx)
+//    {
+//        return arr[idx];
+//    }
+//    void RemoveArray(int idx)
+//    {
+//        for (int i = idx; i < size - 1; ++i)
+//            arr[i] = arr[i + 1];
+//        --size;
+//    }
+//    Array(int cap) // 持失切
+//    {
+//        size = 0;
+//        capacity = cap;
+//        arr = new int[cap];
+//        printf("Array()\n");
+//    }
+//    ~Array()// 社瑚切
+//    {
+//        delete[] arr;
+//        printf("~Array()\n");
+//    }
+//};
+//int main()
+//{
+//    {
+//        Array arr(100);
+//
+//        arr.AddArray(10);
+//        arr.AddArray(20);
+//        arr.AddArray(30);
+//
+//        arr.RemoveArray(1);
+//        for (int i = 0; i < arr.SizeArray(); ++i)
+//            printf("%d\n", arr.AtArray(i));
+//    }
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//class Array
+//{
+//private:
+//    int* arr;
+//    int size;
+//    int capacity;
+//public:
+//    void AddArray(int data)
+//    {
+//        arr[size++] = data;
+//    }
+//    int SizeArray()
+//    {
+//        return size;
+//    }
+//    int AtArray(int idx)
+//    {
+//        return arr[idx];
+//    }
+//    void RemoveArray(int idx)
+//    {
+//        for (int i = idx; i < size - 1; ++i)
+//            arr[i] = arr[i + 1];
+//        --size;
+//    }
+//    void InitArray(int cap)
+//    {
+//        size = 0;
+//        capacity = cap;
+//        arr = new int[cap];
+//    }
+//    void UninitArray()
+//    {
+//        delete[] arr;
+//    }
+//};
+//int main()
+//{
+//    Array arr;
+//
+//    arr.InitArray(100);
+//
+//    arr.AddArray(10);
+//    arr.AddArray(20);
+//    arr.AddArray(30);
+//
+//    arr.RemoveArray(1);
+//    for (int i = 0; i < arr.SizeArray(); ++i)
+//        printf("%d\n", arr.AtArray(i));
+//
+//    arr.UninitArray();
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Array
+//{
+//    int* arr;
+//    int size;
+//    int capacity;
+//    void AddArray(int data)
+//    {
+//        arr[size++] = data;
+//    }
+//    int SizeArray()
+//    {
+//        return size;
+//    }
+//    int AtArray(int idx)
+//    {
+//        return arr[idx];
+//    }
+//    void RemoveArray(int idx)
+//    {
+//        for (int i = idx; i < size - 1; ++i)
+//            arr[i] = arr[i + 1];
+//        --size;
+//    }
+//    void InitArray(int cap)
+//    {
+//        size = 0;
+//        capacity = cap;
+//        arr = new int[cap];
+//    }
+//    void UninitArray()
+//    {
+//        delete[] arr;
+//    }
+//};
+//int main()
+//{
+//    Array arr;
+//
+//    arr.InitArray(100);
+//
+//    arr.AddArray(10);
+//    arr.AddArray(20);
+//    arr.AddArray(30);
+//
+//    arr.RemoveArray(1);
+//    for (int i = 0; i < arr.SizeArray(); ++i)
+//        printf("%d\n", arr.AtArray(i));
+//
+//    arr.UninitArray();
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Array
+//{
+//    int* arr;
+//    int size;
+//    int capacity;
+//};
+//void AddArray(Array& ar, int data)
+//{
+//    ar.arr[ar.size++] = data;
+//}
+//int SizeArray(Array& ar)
+//{
+//    return ar.size;
+//}
+//int AtArray(Array& ar, int idx)
+//{
+//    return ar.arr[idx];
+//}
+//void RemoveArray(Array& ar, int idx)
+//{
+//    for (int i = idx; i < ar.size - 1; ++i)
+//        ar.arr[i] = ar.arr[i + 1];
+//    --ar.size;
+//}
+//void InitArray(Array& ar, int cap)
+//{
+//    ar.size = 0;
+//    ar.capacity = cap;
+//    ar.arr = new int[cap];
+//}
+//void UninitArray(Array& ar)
+//{
+//    delete[] ar.arr;
+//}
+//int main()
+//{
+//    Array arr;
+//
+//    InitArray(arr, 100);
+//
+//    AddArray(arr, 10);
+//    AddArray(arr, 20);
+//    AddArray(arr, 30);
+//
+//    RemoveArray(arr, 1);
+//    for (int i = 0; i < SizeArray(arr); ++i)
+//        printf("%d\n", AtArray(arr, i));
+//
+//    UninitArray(arr);
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Array
+//{
+//    int* arr;
+//    int size;
+//};
+//void AddArray(Array& ar, int data)
+//{
+//    ar.arr[ar.size++] = data;
+//}
+//int SizeArray(Array& ar)
+//{
+//    return ar.size;
+//}
+//int AtArray(Array& ar, int idx)
+//{
+//    return ar.arr[idx];
+//}
+//void RemoveArray(Array& ar, int idx)
+//{
+//    for (int i = idx; i < ar.size - 1; ++i)
+//        ar.arr[i] = ar.arr[i + 1];
+//    --ar.size;
+//}
+//void InitArray(Array& ar, int cap)
+//{
+//    ar.size = 0;
+//    ar.arr = new int[cap];
+//}
+//void UninitArray(Array& ar)
+//{
+//    delete[] ar.arr;
+//}
+//int main()
+//{
+//    Array arr;
+//
+//    InitArray(arr, 100);
+//
+//    AddArray(arr, 10);
+//    AddArray(arr, 20);
+//    AddArray(arr, 30);
+//
+//    RemoveArray(arr, 1);
+//    for (int i = 0; i < SizeArray(arr); ++i)
+//        printf("%d\n", AtArray(arr, i));
+//
+//    UninitArray(arr);
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Array
+//{
+//    int* arr;
+//    int size;
+//};
+//void AddArray(Array& ar, int data)
+//{
+//    ar.arr[ar.size++] = data;
+//}
+//int SizeArray(Array& ar)
+//{
+//    return ar.size;
+//}
+//int AtArray(Array& ar, int idx)
+//{
+//    return ar.arr[idx];
+//}
+//void InitArray(Array& ar, int cap)
+//{
+//    ar.size = 0;
+//    ar.arr = new int[cap];
+//}
+//void UninitArray(Array& ar)
+//{
+//    delete[] ar.arr;
+//}
+//int main()
+//{
+//    Array arr;
+//
+//    InitArray(arr, 100);
+//
+//    AddArray(arr, 10);
+//    AddArray(arr, 20);
+//    AddArray(arr, 30);
+//
+//    for (int i = 0; i < SizeArray(arr); ++i)
+//        printf("%d\n", AtArray(arr, i));
+//
+//    UninitArray(arr);
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Array
+//{
+//    int* arr;
+//    int size;
+//};
+//void AddArray(Array& ar, int data)
+//{
+//    ar.arr[ar.size++] = data;
+//}
+//void PrintArray(Array& ar)
+//{
+//    for (int i = 0; i < ar.size; ++i)
+//        printf("%d\n", ar.arr[i]);
+//}
+//void InitArray(Array& ar, int cap)
+//{
+//    ar.size = 0;
+//    ar.arr = new int[cap];
+//}
+//void UninitArray(Array& ar)
+//{
+//    delete[] ar.arr;
+//}
+//int main()
+//{
+//    Array arr;
+//
+//    InitArray(arr,100);
+//
+//    AddArray(arr, 10);
+//    AddArray(arr, 20);
+//    AddArray(arr, 30);
+//
+//    PrintArray(arr);
+//
+//    UninitArray(arr);
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Array
+//{
+//    int arr[5];
+//    int size;
+//};
+//void AddArray(Array& ar, int data)
+//{
+//    ar.arr[ar.size++] = data;
+//}
+//void PrintArray(Array& ar)
+//{
+//    for (int i = 0; i < ar.size; ++i)
+//        printf("%d\n", ar.arr[i]);
+//}
+//void InitArray(Array& arr)
+//{
+//    arr.size = 0;
+//}
+//void UninitArray(Array& arr)
+//{
+//    //?
+//}
+//int main()
+//{
+//    Array arr;
+//
+//    InitArray(arr);
+//
+//    AddArray(arr, 10);
+//    AddArray(arr, 20);
+//    AddArray(arr, 30);
+//
+//    PrintArray(arr);
+//
+//    UninitArray(arr);
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Array
+//{
+//    int arr[5];
+//    int size;
+//};
+//void AddArray(Array& ar, int data)
+//{
+//    ar.arr[ar.size++] = data;
+//}
+//void PrintArray(Array& ar)
+//{
+//    for (int i = 0; i < ar.size; ++i)
+//        printf("%d\n", ar.arr[i]);
+//}
+//int main()
+//{
+//    Array arr = { 0 };
+//
+//    AddArray(arr, 10);
+//    AddArray(arr, 20);
+//    AddArray(arr, 30);
+//
+//    PrintArray(arr);
+//
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//void AddArray(int* arr, int& size, int data)
+//{
+//    arr[size++] = data;
+//}
+//void PrintArray(int* arr, int size)
+//{
+//    for (int i = 0; i < size; ++i)
+//        printf("%d\n", arr[i]);
+//}
+//int main()
+//{
+//    int arr[5] = { 0 };
+//    int size = 0;
+//
+//    AddArray(arr, size, 10);
+//    AddArray(arr, size, 20);
+//    AddArray(arr, size, 30);
+//
+//    PrintArray(arr, size);
+//
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//int main()
+//{
+//    int arr[5] = { 0 };
+//    int size = 0;
+//
+//    arr[size++] = 10;
+//    arr[size++] = 20;
+//    arr[size++] = 30;
+//
+//    for (int i = 0; i < size; ++i)
+//        printf("%d\n", arr[i]);
+//
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//int main()
+//{
+//    int* arr = new int[5];
+//
+//    int n = 10;
+//    arr = &n;
+//
+//    for (int i = 0; i < 5; ++i)
+//        arr[i] = 10 * (i + 1);
+//    for (int i = 0; i < 5; ++i)
+//        printf("%d\n", arr[i]);
+//
+//    delete[] arr;
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//int main()
+//{
+//    int* arr = new int[5];
+//    
+//    for (int i = 0; i < 5; ++i)
+//        arr[i] = 10 * (i + 1);
+//    for (int i = 0; i < 5; ++i)
+//        printf("%d\n", arr[i]);
+//
+//    delete[] arr;
+//    system("pause");
+//}
+
+//#include <cstdio>
+//#include <cstdlib>
+//void ArrayTest()
+//{
+//    int arr[5] = { 10,20,30,40,50 };
+//    for (int i = 0; i < 5; ++i)
+//        printf("%d\n", arr[i]);
+//}
+//int main()
+//{
+//    ArrayTest();
+//    ArrayTest();
+//    ArrayTest();
+//
+//    system("pause");
 //}
--- ex04/Main.cpp
+++ ex04/Main.cpp
@@ -1,50 +1,400 @@
 #include <cstdio>
 #include <cstdlib>
+
 struct Node {
 	int data;
 	Node* prev;
 	Node* next;
 };
+class List {
+private:
+	Node* head;
+	Node* tail;
 
-Node* AllocNode(int data) {
+	Node* AllocNode(int data) {
 
-	Node* p = new Node;
-	p->data = data;
-	p->prev = p->next = NULL;
+		Node* p = new Node;
+		p->data = data;
+		p->prev = p->next = NULL;
 
-	return p;
-}
+		return p;
+	}
+
+public:
+
+	void AddList(int data) {
+		Node* p = AllocNode(data);
+		Node* ptail = tail->prev;
+		ptail->next = p;
+		p->prev = ptail;
+		p->next = tail;
+		tail->prev = p;
+	}
+
+	void PirntNextLst() {
+
+		for (Node* p = head->next; p != tail; p = p->next)
+			printf("%d\n", p->data);
+	}
+
+	void PrintPrevList() {
+		for (Node* p = tail->prev; p != head; p = p->prev)
+			printf("%d\n", p->data);
+
+	}
+	List() {
+		head = AllocNode(0);
+		tail = AllocNode(0);
+		head->next = tail;
+		tail->prev = head;
+
+	}
+	~List() {
+		for (Node* p = head; p != NULL;) {
+			Node* np = p->next;
+			delete p;
+			p = np;
+		}
+	}
+
+
+};
 int main() {
 
-	Node* head = AllocNode(0);
-	Node* tail = AllocNode(0);
-	head->next =tail;
-	tail->prev = head;
+	List lt;
 
 
-	Node* p = AllocNode(10);
-	head = tail = p;
+	lt.AddList(10);
+	lt.AddList(20);
+	lt.AddList(30);
 
-	p = AllocNode(20);
-	tail->next = p;
-	p->prev = tail;
-	tail = p;
+	lt.PirntNextLst();
 
-	p = AllocNode(30);
-	tail->next = p;
-	p->prev = tail;
-	tail = p;
-
-	printf("%d\n", head->data);
-	printf("%d\n", head->next->data);
-	printf("%d\n", head->next->next->data);
-
-	printf("%d\n", tail->data);
-	printf("%d\n", tail->prev->data);
-	printf("%d\n", tail->prev->prev->data);
+	lt.PrintPrevList();
 
 	system("pause");
 }
+
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Node {
+//	int data;
+//	Node* prev;
+//	Node* next;
+//};
+//struct List {
+//private:
+//	Node* head;
+//	Node* tail;
+//	
+//	Node* AllocNode(int data) {
+//
+//		Node* p = new Node;
+//		p->data = data;
+//		p->prev = p->next = NULL;
+//
+//		return p;
+//	}
+//
+//public:
+//
+//	void AddList(int data) {
+//		Node* p = AllocNode(data);
+//		Node* ptail = tail->prev;
+//		ptail->next = p;
+//		p->prev = ptail;
+//		p->next = tail;
+//		tail->prev = p;
+//	}
+//
+//	void PirntNextLst() {
+//
+//		for (Node* p = head->next; p != tail; p = p->next)
+//			printf("%d\n", p->data);
+//	}
+//
+//	void PrintPrevList() {
+//		for (Node* p = tail->prev; p != head; p = p->prev)
+//			printf("%d\n", p->data);
+//
+//	}
+//	void InitList() {
+//		head = AllocNode(0);
+//		tail = AllocNode(0);
+//		head->next = tail;
+//		tail->prev = head;
+//
+//	}
+//	void UninitList() {
+//		for (Node* p = head; p != NULL;) {
+//			Node* np = p->next;
+//			delete p;
+//			p = np;
+//		}
+//	}
+//};
+//int main() {
+//
+//	List lt;
+//
+//	lt.InitList();
+//
+//	lt.AddList(10);
+//	lt.AddList(20);
+//	lt.AddList(30);
+//
+//	lt.PirntNextLst();
+//
+//	lt.PrintPrevList();
+//	lt.UninitList();
+//
+//	system("pause");
+//}
+
+
+//#include <cstdio>
+//#include <cstdlib>
+//
+//struct Node {
+//	int data;
+//	Node* prev;
+//	Node* next;
+//};
+//struct List {
+//	Node* head;
+//	Node* tail;
+//};
+//Node* AllocNode(int data) {
+//
+//	Node* p = new Node;
+//	p->data = data;
+//	p->prev = p->next = NULL;
+//
+//	return p;
+//}
+//void AddList(List& lt, int data) {
+//	Node* p = AllocNode(data);
+//	Node* ptail = lt.tail->prev;
+//	ptail->next = p;
+//	p->prev = ptail;
+//	p->next = lt.tail;
+//	lt.tail->prev = p;
+//}
+//
+//void PirntNextLst(List& lt) {
+//
+//	for (Node* p = lt.head->next; p != lt.tail; p = p->next)
+//		printf("%d\n", p->data);
+//}
+//
+//void PrintPrevList(List& lt) {
+//	for (Node* p = lt.tail->prev; p != lt.head; p = p->prev)
+//		printf("%d\n", p->data);
+//
+//}
+//void InitList(List& lt) {
+//	lt.head = AllocNode(0);
+//	lt.tail = AllocNode(0);
+//	lt.head->next = lt.tail;
+//	lt.tail->prev = lt.head;
+//
+//}
+//void UninitList(List& lt) {
+//	for (Node* p = lt.head; p != NULL;) {
+//		Node* np = p->next;
+//		delete p;
+//		p = np;
+//	}
+//}
+//
+//int main() {
+//
+//	List lt;
+//
+//	InitList(lt);
+//
+//
+//	AddList(lt, 10);
+//	AddList(lt, 20);
+//	AddList(lt, 30);
+//
+//	PirntNextLst(lt);
+//
+//	PrintPrevList(lt);
+//	UninitList(lt);
+//
+//	system("pause");
+//}
+
+
+//#include <cstdio>
+//#include <cstdlib>
+//struct Node {
+//	int data;
+//	Node* prev;
+//	Node* next;
+//};
+//struct List {
+//	Node* head;
+//	Node* tail;
+//};
+//Node* AllocNode(int data) {
+//
+//	Node* p = new Node;
+//	p->data = data;
+//	p->prev = p->next = NULL;
+//
+//	return p;
+//}
+//void AddNode(List& lt, int data) {
+//	Node* p = AllocNode(data);
+//	Node* ptail = lt.tail->prev;
+//	ptail->next = p;
+//	p->prev = ptail;
+//	p->next = lt.tail;
+//	lt.tail->prev = p;
+//}
+//
+//void PirntNextLst(List& lt) {
+//	printf("%d\n", lt.head->next->data);
+//	printf("%d\n", lt.head->next->next->data);
+//	printf("%d\n", lt.head->next->next->next->data);
+//}
+//
+//void PrintPrevList(List& lt) {
+//	printf("%d\n", lt.tail->prev->data);
+//	printf("%d\n", lt.tail->prev->prev->data);
+//	printf("%d\n", lt.tail->prev->prev->prev->data);
+//}
+//int main() {
+//
+//	List lt;
+//	lt.head = AllocNode(0);
+//	lt.tail = AllocNode(0);
+//
+//	lt.head->next = lt.tail;
+//	lt.tail->prev = lt.head;
+//
+//
+//	AddNode(lt, 10);
+//	AddNode(lt, 20);
+//	AddNode(lt, 30);
+//
+//	PirntNextLst(lt);
+//
+//	PrintPrevList(lt);
+//
+//
+//	system("pause");
+//}
+
+
+//#include <cstdio>
+//#include <cstdlib>
+//struct Node {
+//	int data;
+//	Node* prev;
+//	Node* next;
+//};
+//
+//Node* AllocNode(int data) {
+//
+//	Node* p = new Node;
+//	p->data = data;
+//	p->prev = p->next = NULL;
+//
+//	return p;
+//}
+//void AddNode(Node* head, Node* tail, int data) {
+//	Node* p = AllocNode(data);
+//	Node* ptail = tail->prev;
+//	ptail->next = p;
+//	p->prev = ptail;
+//	p->next = tail;
+//	tail->prev = p;
+//}
+//int main() {
+//
+//	Node* head = AllocNode(0);
+//	Node* tail = AllocNode(0);
+//	head->next = tail;
+//	tail->prev = head;
+//
+//
+//	AddNode(head, tail, 10);
+//	AddNode(head, tail, 20);
+//	AddNode(head, tail, 30);
+//
+//	printf("%d\n", head->next->data);
+//	printf("%d\n", head->next->next->data);
+//	printf("%d\n", head->next->next->next->data);
+//
+//	printf("%d\n", tail->prev->data);
+//	printf("%d\n", tail->prev->prev->data);
+//	printf("%d\n", tail->prev->prev->prev->data);
+//
+//	system("pause");
+//}
+
+
+//#include <cstdio>
+//#include <cstdlib>
+//struct Node {
+//	int data;
+//	Node* prev;
+//	Node* next;
+//};
+//
+//Node* AllocNode(int data) {
+//
+//	Node* p = new Node;
+//	p->data = data;
+//	p->prev = p->next = NULL;
+//
+//	return p;
+//}
+//int main() {
+//
+//	Node* head = AllocNode(0);
+//	Node* tail = AllocNode(0);
+//	head->next = tail;
+//	tail->prev = head;
+//
+//
+//	Node* p = AllocNode(10);
+//	Node* ptail = tail->prev;
+//	ptail->next = p;
+//	p->prev = ptail;
+//	p->next = tail;
+//	tail->prev = p;
+//
+//
+//	p = AllocNode(20);
+//	ptail = tail->prev;
+//	ptail->next = p;
+//	p->prev = ptail;
+//	p->next = tail;
+//	tail->prev = p;
+//
+//	p = AllocNode(30);
+//	ptail = tail->prev;
+//	ptail->next = p;
+//	p->prev = ptail;
+//	p->next = tail;
+//	tail->prev = p;
+//
+//	printf("%d\n", head->next->data);
+//	printf("%d\n", head->next->next->data);
+//	printf("%d\n", head->next->next->next->data);
+//
+//	printf("%d\n", tail->prev->data);
+//	printf("%d\n", tail->prev->prev->data);
+//	printf("%d\n", tail->prev->prev->prev->data);
+//
+//	system("pause");
+//}
 
 
 //#include <cstdio>
