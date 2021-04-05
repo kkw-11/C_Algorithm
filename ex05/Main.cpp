7d4f34fcba9663ef28135293ae7af217f3f3b8d0
asus <kkw-11@naver.com>
2021-04-05 19:18:51 +0900

    4/5 Recursion Fibonacci

--- ex05/Main.cpp
+++ ex05/Main.cpp
@@ -1,27 +1,152 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Print(int n) {
+void Fibonacci(int n, int& result) {
 
-	//+는 트리로 말하면 깊어지면서 출력
-	//재귀함수 쌓아갈때 출력
-	printf("+%d\n", n);
-	if (n > 0) {
-		Print(n - 1);
-		Print(n - 1);
+	if (n == 1)
+		result = 1;
+	else if (n == 2)
+		result = 2;
+	else {
+		int f1, f2;
+		Fibonacci(n - 1, f1);
+		Fibonacci(n - 2, f2);
+		result = f1 + f2;
+
 	}
-	//-는 거꾸로 빠져나올 때
-	printf("-%d\n", n);
 
 }
 
 int main() {
 
-	Print(3);
+	int fibo;
+
+	Fibonacci(4, fibo);
+	printf("%d\n", fibo);
+
 	system("pause");
 
 	return 0;
 }
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int Fibonacci(int n) {
+//	
+//	if (n == 1)
+//		return 1;
+//	else if (n == 2)
+//		return 2;
+//	else
+//		return Fibonacci(n - 1) + Fibonacci(n - 2);
+//
+//}
+//
+//int main() {
+//
+//	printf("%d\n", Fibonacci(5));
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void Factorial(int n, int& result) {
+//
+//	if (n == 1)
+//		result = 1;
+//	else {
+//		Factorial(n - 1, result);
+//		result = n * result;
+//
+//	}
+//}
+//
+//int main() {
+//
+//	int fact = 0;
+//	Factorial(5, fact);
+//	printf("%d\n", fact);
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void Factorial(int n, int& result) {
+//
+//	if (n == 1)
+//		result = 1;
+//	else {
+//		int nFact;
+//		Factorial(n - 1,nFact);
+//		result = n * nFact;
+//
+//	}
+//}
+//
+//int main() {
+//
+//	int fact = 0;
+//	Factorial(5, fact);
+//	printf("%d\n", fact);
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void Factorial(int n, int& result) {
+//
+//	result = 100; //fact에 100을 넣음 아웃파라미터 이용
+//}
+//
+//int main() {
+//
+//	int fact = 0;
+//	Factorial(5, fact);
+//	printf("%d\n", fact);
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int Factorial(int n) {
+//
+//	if (n == 1)
+//		return 1;
+//	else
+//		return n * Factorial(n - 1);
+//
+//}
+//
+//int main() {
+//
+//	printf("%d\n", Factorial(5));
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
 
 //#include <stdio.h>
