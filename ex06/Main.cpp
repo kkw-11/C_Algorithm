f64b11e2bcab4028f8181bc741541b9bf1720be4
asus <kkw-11@naver.com>
2021-04-07 23:38:54 +0900

    quick

--- ex06/Main.cpp
+++ ex06/Main.cpp
@@ -6,24 +6,36 @@ void Swap(int& a, int& b) {
 	b = t;
 }
 
-void Sort(int list[], int size) {
+void _Sort(int list[], int left, int right) {
 
-	for (int idx = 1; idx < size; ++idx)
-	{
+	if (left <= right) { 
+		int pivot = left;
+		int i = left + 1;
+		int j = right;
+		while (i <= j)
+		{
+			while (list[pivot] > list[i])
+				++i;
+			while (list[pivot] < list[j])
+				--j;
 
-		int value = list[idx];
-
-		int i;
-		for (i = idx - 1; 0 <= i; --i) {
-			if (value < list[i])
-				list[i + 1] = list[i];
+			if (i <= j)
+				Swap(list[i], list[j]);
 			else
 				break;
 		}
-		list[i + 1] = value;
-
+		Swap(list[pivot], list[j]);
+		_Sort(list, left, j - 1);
+		_Sort(list, j + 1, right);
 	}
 
+}
+//Sort는 퀵소트의 재귀적인 구현을 위한 매개변수를 모두 갖고 있지 않으므로
+//위임함수를 만들어서 동작 시킴
+//
+void Sort(int list[], int size) {
+
+	_Sort(list, 0, size - 1);
 }
 void PrintList(int list[], int size) {
 
@@ -34,8 +46,8 @@ void PrintList(int list[], int size) {
 }
 
 int main() {
-	int list[10] = { 40,30,10,70,50,80,90,60 };
-	//	int list[10] = { 10,30,40,70,5,80,90,60 };
+	//int list[10] = { 40,30,10,70,50,80,90,60 };
+	int list[10] = { 40,30,70,90,50,80,10,60 };
 
 
 	PrintList(list, 8);
@@ -45,6 +57,172 @@ int main() {
 	system("pause");
 
 }
+
+//#include <cstdio>
+//#include <cstdlib>
+//void Swap(int& a, int& b) {
+//	int t = a;
+//	a = b;
+//	b = t;
+//}
+//
+//void Sort(int list[], int size) {
+//
+//	for (int j = 0; j < size - 1; ++j)
+//	{
+//		for (int i = 0; i < (size - 1) - j; ++i) {
+//			if (list[i] > list[i + 1])
+//				Swap(list[i], list[i + 1]);
+//		}
+//
+//	}
+//}
+//void PrintList(int list[], int size) {
+//
+//	for (int i = 0; i < size; ++i) {
+//		printf("%5d", list[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main() {
+//	//int list[10] = { 40,30,10,70,50,80,90,60 };
+//	int list[10] = { 40,30,70,90,50,80,10,60 };
+//
+//
+//	PrintList(list, 8);
+//	Sort(list, 8);
+//	PrintList(list, 8);
+//
+//	system("pause");
+//
+//}
+
+
+
+//#include <cstdio>
+//#include <cstdlib>
+//void Swap(int& a, int& b) {
+//	int t = a;
+//	a = b;
+//	b = t;
+//}
+//
+//void Sort(int list[], int size) {
+//
+//	for (int j = 0; j < size - 1; ++j)
+//	{
+//		for (int i = 0; i < (size - 1) - j; ++i) {
+//			if (list[i] > list[i + 1])
+//				Swap(list[i], list[i + 1]);
+//		}
+//
+//	}
+//}
+//void PrintList(int list[], int size) {
+//
+//	for (int i = 0; i < size; ++i) {
+//		printf("%5d", list[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main() {
+//	int list[10] = { 40,30,10,70,50,80,90,60 };
+//
+//
+//	PrintList(list, 8);
+//	Sort(list, 8);
+//	PrintList(list, 8);
+//
+//	system("pause");
+//
+//}
+//
+
+////#include <cstdio>
+//#include <cstdlib>
+//void Swap(int& a, int& b) {
+//	int t = a;
+//	a = b;
+//	b = t;
+//}
+//
+//void Sort(int list[], int size) {
+//
+//	for (int i = 0; i < size-1; ++i) {
+//		if (list[i] > list[i + 1])
+//			Swap(list[i], list[i + 1]);
+//	}
+//}
+//void PrintList(int list[], int size) {
+//
+//	for (int i = 0; i < size; ++i) {
+//		printf("%5d", list[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main() {
+//	int list[10] = { 40,30,10,70,50,80,90,60 };
+//
+//
+//	PrintList(list, 8);
+//	Sort(list, 8);
+//	PrintList(list, 8);
+//
+//	system("pause");
+//
+//}
+
+
+//#include <cstdio>
+//#include <cstdlib>
+//void Swap(int& a, int& b) {
+//	int t = a;
+//	a = b;
+//	b = t;
+//}
+//
+//void Sort(int list[], int size) {
+//
+//	for (int idx = 1; idx < size; ++idx)
+//	{
+//
+//		int value = list[idx];
+//
+//		int i;
+//		for (i = idx - 1; 0 <= i; --i) {
+//			if (value < list[i])
+//				list[i + 1] = list[i];
+//			else
+//				break;
+//		}
+//		list[i + 1] = value;
+//
+//	}
+//
+//}
+//void PrintList(int list[], int size) {
+//
+//	for (int i = 0; i < size; ++i) {
+//		printf("%5d", list[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main() {
+//	int list[10] = { 40,30,10,70,50,80,90,60 };
+//	//	int list[10] = { 10,30,40,70,5,80,90,60 };
+//
+//
+//	PrintList(list, 8);
+//	Sort(list, 8);
+//	PrintList(list, 8);
+//
+//	system("pause");
+//
+//}
 
 //#include <cstdio>
 //#include <cstdlib>
